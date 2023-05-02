eb7d3aa06d653ded8947f47d1789658aa75f4781
Dell <Dell@DESKTOP-9T5M8TE>
2023-05-01 21:23:43 -0700

    Feat : Game draw

--- src/main/java/com/game/TictactoeApplication.java
+++ src/main/java/com/game/TictactoeApplication.java
@@ -13,32 +13,32 @@ import com.game.service.BoardGame;
 import com.game.util.ApplicationConstant;
 
 @SpringBootApplication
-public class TictactoeApplication implements CommandLineRunner{
-	
+public class TictactoeApplication implements CommandLineRunner {
+
 	private static final Logger logger = LoggerFactory.getLogger(TictactoeApplication.class);
-	
+
 	@Autowired
 	private BoardGame boardGame;
-	
-	public static void main(String[] args)  {
+	private String board[][] = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+
+	public static void main(String[] args) {
 		SpringApplication.run(TictactoeApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 
-		String result =playGame();
+		String result = playGame();
 		logger.info(result);
 	}
 
-	private String playGame() {
+	public String playGame() {
 
 		String result = null;
-		String board[][] = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
-		Map<String, String> playerMap = boardGame.playGame(board);
+		Map<String, String> playerMap = boardGame.playGame(getBoard());
 
 		if (playerMap.size() == 1) {
-			
+
 			result = "Match draws between players";
 		} else {
 			result = "Player " + playerMap.get(ApplicationConstant.PLAYER_NAME) + " wins the game.";
@@ -47,6 +47,8 @@ public class TictactoeApplication implements CommandLineRunner{
 		return result;
 	}
 
-
+	public String[][] getBoard() {
+		return board;
+	}
 
 }
--- src/test/java/com/game/TictactoeApplicationTests.java
+++ src/test/java/com/game/TictactoeApplicationTests.java
@@ -1,13 +1,49 @@
 package com.game;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-@SpringBootTest
+import com.game.service.BoardGame;
+import com.game.util.ApplicationConstant;
+
 class TictactoeApplicationTests {
-
-	@Test
-	void contextLoads() {
+	
+	@InjectMocks
+	private TictactoeApplication application;
+	
+	@Mock
+	private BoardGame game;
+	private String draw ="Match draws between players";
+	
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.openMocks(this);
 	}
+	
+	@Test
+	@DisplayName("game draw between players")
+	public void gameDraw() {
+		
+		Map<String, String> map = new HashMap<>();
+		map.put(ApplicationConstant.GAME, ApplicationConstant.DRAW);
+		
+		doReturn(map).when(game).playGame(application.getBoard());
+		
+		String result = application.playGame();
+		
+		assertEquals(draw, result);
+	}
+	
+
 
 }
